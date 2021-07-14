6711bf1aec7e308dd7aa27c701211fddf3006fb0
welltrojan <wellingtontrojan@gmail.com>
2021-07-14 15:36:23 -0300

    Componentes com TypeScript

--- reactjs/01-github-explorer/src/componnents/RepositoryList.tsx
+++ reactjs/01-github-explorer/src/componnents/RepositoryList.tsx
@@ -5,8 +5,14 @@ import '../styles/repositories.sass'
 
 const repositoryName = 'Lista de Repositórios';
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/users/welltrojan/repos')
