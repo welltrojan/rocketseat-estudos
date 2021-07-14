e48d4440c8c0eb3964e681e37f6b5c60532e2685
welltrojan <wellingtontrojan@gmail.com>
2021-07-14 14:46:18 -0300

    Listando repositórios

--- reactjs/01-github-explorer/src/componnents/RepositoryItem.jsx
+++ reactjs/01-github-explorer/src/componnents/RepositoryItem.jsx
@@ -3,7 +3,7 @@ export function RepositoryItem(props) {
         <li>
             <strong>{props.repository?.name ?? 'Default'}</strong>
             <p>{props.repository.description}</p>
-            <a href={props.repository.link}>Ver Repositorio</a>
+            <a href={props.repository.html_url}>Ver Repositorio</a>
         </li>
     );
 }
--- reactjs/01-github-explorer/src/componnents/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/componnents/RepositoryList.jsx
@@ -4,12 +4,6 @@ import { useState, useEffect } from "react";
 
 const repositoryName = 'Lista de Repositórios';
 
-const repository = {
-    name: 'Nome Rep',
-    description: 'Lorem Ipsum',
-    link: 'https://github.com/welltrojan'
-}
-
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
 
@@ -23,9 +17,9 @@ export function RepositoryList() {
         <section className="repository-list">
             <h1>{repositoryName}</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     )
